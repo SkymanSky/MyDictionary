1925e6cd2847acc3478a2ddb9a836ddb88a50b00
SkymanSky <gokera@ogr.iu.edu.tr>
2021-03-27 23:18:56 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<TKeys, TValues>
+    {
+        TKeys[] Keys;
+        TValues[] Values;
+        public MyDictionary()
+        {
+            Keys = new TKeys[0];
+            Values = new TValues[0];
+
+        }
+
+        public void Add(TKeys Key, TValues Value)
+        {
+            TKeys[] temporaryKeys = Keys;
+            TValues[] temporaryValues = Values;
+
+            Keys = new TKeys[Keys.Length + 1];
+            Values = new TValues[Values.Length + 1];
+
+            for (int i = 0; i < temporaryKeys.Length; i++)
+            {
+                Keys[i] = temporaryKeys[i];
+            }
+
+            for (int i = 0; i < temporaryValues.Length; i++)
+            {
+                Values[i] = temporaryValues[i];
+            }
+
+            Keys[Keys.Length - 1] = Key;
+            Values[Values.Length - 1] = Value;
+        }
+
+        public TValues GetValuesByKeys(TKeys Key)
+        {
+            int index = Array.IndexOf(Keys, Key);
+            return Values[index];
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MyDictionary;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyDictionary<string, string> studentsList = new MyDictionary<string, string>();
+            studentsList.Add("1", "Ali");
+            studentsList.Add("2", "Hakan");
+            studentsList.Add("3", "Ayşe");
+
+            Console.WriteLine(studentsList.GetValuesByKeys("1"));
+            Console.WriteLine(studentsList.GetValuesByKeys("2"));
+            Console.WriteLine(studentsList.GetValuesByKeys("3"));
+
+        }
+    }
+}
